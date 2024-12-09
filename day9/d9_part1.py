26e23ff2d62eeacd073ad0c00cb242a24829d31d
Jiajia Chen <jiajchen@f1r6s9.codam.nl>
2024-12-09 18:50:36 +0100

    day9

---
+++ day9/d9_part1.py
@@ -0,0 +1,51 @@
+import sys
+
+with open(sys.argv[1], 'r') as file:
+	disk_map : str = file.readline().strip()
+
+def expand_block() -> list[str]:
+	file = []
+	id = 0
+	for i, c in enumerate(disk_map):
+		sz = int(c)
+		if i % 2 == 0:
+			file += [str(id)] * sz
+			id += 1
+		else:
+			file += ["."] * sz
+	return file
+
+def expand_file() -> list[str]:
+	file = []
+	id = 0
+	for i, c in enumerate(disk_map):
+		sz = int(c)
+		if i % 2 == 0:
+			file += [[str(id)] * sz]
+			id += 1
+		else:
+			file += ["."] * sz
+	return file
+
+def move_file_blocks(file : list[str]) -> list[str]:
+	for i in range(len(file) - 1, -1, -1):
+		if file[i] == ".":
+			continue
+		id = file.index(".")
+		if id > i:
+			break
+		file[id], file[i] = file[i], "."
+	return file
+
+def filesystem_checksum(filesystem : list[str]) -> int:
+	res = 0
+	for i, c in enumerate(filesystem):
+		if c == ".":
+			break
+		res += i * int(c)
+	return res
+
+# print(expand_block())
+print("part 1: ", filesystem_checksum(move_file_blocks(list(expand_block()))))
+
+# print("part 2: ", get_calibration_result_new())
---
+++ day9/d9_part2.py
@@ -0,0 +1,37 @@
+import sys
+
+with open(sys.argv[1], 'r') as file:
+	disk_map : str = file.readline().strip()
+
+def expand_file() -> list:
+	file = []
+	id = 0
+	for i, c in enumerate(disk_map):
+		sz = int(c)
+		if i % 2 == 0:
+			file += [[str(id)] * sz]
+			id += 1
+		else:
+			file += ["."] * sz
+	return file
+
+def move_file_blocks(file : list) -> list:
+	for i in range(len(file) - 1, -1, -1):
+
+		if file[i] == ".":
+			continue
+		id = file.index("." * )
+		if id > i:
+			break
+		file[id], file[i] = file[i], "."
+	return file
+
+def filesystem_checksum(filesystem : list[str]) -> int:
+	res = 0
+	for i, c in enumerate(filesystem):
+		if c == ".":
+			break
+		res += i * int(c)
+	return res
+
+print("part 2: ", )
